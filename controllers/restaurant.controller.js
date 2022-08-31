2e7d110eeec12244accc8ed8fc3b81f821aba4f4
Kushal Mondal <kushal99mondal@gmail.com>
2022-08-31 16:33:37 +0530

    Getting restaurants by Category,Id,RatingValue

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -54,12 +54,74 @@ exports.getCategories = async(req,res) => {
         const restaurant_category = await Restaurant.distinct("category");
         res.status(200).send(restaurant_category);
     }catch(err){
-        console.log("Some error occured while creating the products", err.message);
+        console.log("Some error while", err.message);
         res.status(500).send({
             message: "Some error occurred while fetching Categories"
         });
    }
 }
 
+
+exports.getRestaurantByCategory = async(req, res) => {
+    
+    try{
+
+        const restaurant = await Restaurant.find({category : req.params.category});
+        if(!restaurant){
+            return res.status(404).send({});
+        }
+        res.status(200).send(restaurant);
+
+    }catch(err){
+        console.log("Some error while", err.message);
+        res.status(500).send({
+            message: "Some error occured while fetching the Restaurant"
+        });
+    }
+}
+
+
+exports.getRestaurantsById = async(req, res) => {
+    try{
+
+        const restaurant = await Restaurant.findOne({_id : req.params.id});
+        if(!restaurant){
+            return res.status(404).send({
+                message : "No Restaurant found with the given ID"
+            });
+        }
+
+        res.status(200).send(restaurant);
+
+    }catch(err){
+        console.log("Some error while", err.message);
+        res.status(500).send({
+            message :  "Some error occured while fetching the Restaurant"
+        })
+    }
+}
+
+exports.getRestaurantsByRating = async(req, res) => {
+    try{
+
+        
+        const restaurant = await Restaurant.find({rating:{$gte:req.params.ratingValue}});
+        
+        if(!restaurant){
+            return res.status(404).send({
+                message : "No Restaurant found with the given Rating or more than that"
+            });
+        }
+        
+
+        res.status(200).send(restaurant);
+
+    }catch(err){
+        console.log("Some error while", err.message);
+        res.status(500).send({
+            message :  "Some error occured while fetching the Restaurant"
+        })
+    }
+}
 
 
--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -11,5 +11,12 @@ module.exports = (app) => {
     //This is the route for all the Restaurant's Categories
     app.get("/fmr/api/restaurant/categories", restaurant_controller.getCategories);
 
+    //This is the route for restaurants with categories
+    app.get("/fmr/api/restaurant/categories/:category", restaurant_controller.getRestaurantByCategory);
 
+    //This is the route for getting the restaurant by ID
+    app.get("/fmr/api/restaurant/:id", restaurant_controller.getRestaurantsById);
+
+    //This is the route for getting the restaurant by Rating
+    app.get("/fmr/api/restaurant/rating/:ratingValue", restaurant_controller.getRestaurantsByRating);
 }
